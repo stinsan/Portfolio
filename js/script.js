e004c7e17e2448f65b81723ea01d87014590575d
Justin Santos <stinsan@ou.edu>
2019-11-03 13:44:18 -0600

    Updates

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const justinsantosText = "justinsantos";
+const midNameTypingDelay = 2000;
+const onLoadTypingDelay = 1500;
+const typingDelay = 125;
+const juSpan = document.querySelector(".ju");
+const stinsanSpan = document.querySelector(".stinsan");
+const tosSpan = document.querySelector(".tos");
+const blinkingUnderscoreSpan = document.querySelector(".blinking-underscore");
+let charIndex = -1;
+
+function typeHomepage() {
+  if (charIndex < justinsantosText.length) {
+    // Currently typing, remove blinking underscore
+    if (!blinkingUnderscoreSpan.classList.contains("typing")) {
+      blinkingUnderscoreSpan.classList.add("typing");
+    }
+
+    charIndex++;
+
+    // Add next character
+    if (charIndex < 2) {
+      juSpan.textContent += justinsantosText.charAt(charIndex);
+    }
+    else if (charIndex >= 2 && charIndex < 9) {
+      stinsanSpan.textContent += justinsantosText.charAt(charIndex);
+    }
+    else {
+      tosSpan.textContent += justinsantosText.charAt(charIndex);
+    }
+
+    if (charIndex === 5) { // Add significant delay between first and last name
+      setTimeout(typeHomepage, midNameTypingDelay);
+      blinkingUnderscoreSpan.classList.remove("typing");
+    }
+    else { // Else normal delay
+      setTimeout(typeHomepage, typingDelay);
+    }
+  }
+  else { // No longer typing, bring back blinking underscore
+    blinkingUnderscoreSpan.classList.remove("typing");
+    setTimeout(stinsanRecolor, 1000);
+  }
+}
+
+function stinsanRecolor() {
+  stinsanSpan.style.color = "#70A1D7";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(typeHomepage, onLoadTypingDelay);
+});
