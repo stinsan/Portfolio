1c80ee0fe7be7f7901964a359576d52ade74fab5
Justin Santos <stinsan@ou.edu>
2019-11-04 01:03:24 -0600

    Updates

---
+++ php/contact.php
@@ -0,0 +1,14 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $email = 'stinsan@ou.edu';
+  $email_subject = "Message from stinsan.com";
+  $email_body = "Name: $name.\n". "Email: $email_from.\n". "Message: $message.\n";
+
+
+
+
+
+?>
