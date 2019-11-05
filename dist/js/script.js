7e7e4e9eaa78f7fb33a6f042e1b3ca090921444e
Justin Santos <stinsan@ou.edu>
2019-11-04 19:43:37 -0600

    Added my sorting project to project list.

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,4 @@
+//--------------------------homepage typewriter---------------------------------
 const justinsantosText = "justinsantos";
 const midNameTypingDelay = 2250;
 const onLoadTypingDelay = 1500;
@@ -46,18 +47,46 @@ function typeHomepage(index) {
   }
 }
 
-function typeHeading(headingName, className) {
-  element = document.querySelector(".className");
-
-  charIndex2++;
-  if (charIndex < headingName.length) {
-    element.textContent += headingName.charAt(charIndex2);
-    setTimeout(typeHeading, typingDelay);
-  }
-}
-
 document.addEventListener("DOMContentLoaded", function() {
   setTimeout(function() {
     typeHomepage(-1)
   }, onLoadTypingDelay);
 });
+
+//-------------------------------modals----------------------------------------
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active")
+  overlay.classList.add("active")
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove("active")
+  overlay.classList.remove("active")
+}
